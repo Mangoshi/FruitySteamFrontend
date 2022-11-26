d5a7d09909a48d3cf6b5166e3ba16126dfb45c24
mangoshi <soggymoose@gmail.com>
2022-11-26 14:50:11 +0000

    Updated home styling, added login/register buttons
    - Added a new 'window' for website information to live in
    - Added login/registration buttons to home card

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,15 @@
-import {Window, WindowContent, WindowHeader} from "react95";
+import {Button, Window, WindowContent, WindowHeader} from "react95";
 import mangowsWave from './images/img_mangowsWave.png'
 
 const Home = () => {
     return (
-        <div style={{display: "flex", justifyContent: 'center'}}>
+        <div>
+        <div style={{display: "flex", justifyContent: 'center', marginBottom: '1rem'}}>
             <Window style={{width: "auto"}}>
-                <WindowHeader>Home.exe</WindowHeader>
+                <WindowHeader style={{display: "flex", justifyContent: 'space-between'}}>
+                    <span>Home.exe</span>
+                    <Button style={{marginTop: '0.2rem'}}>X</Button>
+                </WindowHeader>
                 <WindowContent>
                     <p align='center' style={{fontSize: '1.5rem'}}>
                             Welcome to
@@ -20,8 +24,27 @@ const Home = () => {
                     <div style={{display: "flex", justifyContent: 'center'}}>
                         <img src={mangowsWave} alt='logo'/>
                     </div>
+                    <p align='center' style={{fontSize: '1rem'}}>
+                        Before you begin, you should login or make an account!
+                    </p>
+                    <div align='center' style={{marginTop: '1rem'}}>
+                        <Button style={{margin: '0.5rem'}}>Login</Button>
+                        <Button style={{margin: '0.5rem'}}>Register</Button>
+                    </div>
                 </WindowContent>
             </Window>
+        </div>
+            <div style={{display: "flex", justifyContent: 'center'}}>
+                <Window style={{width: "auto"}}>
+                    <WindowHeader style={{display: "flex", justifyContent: 'space-between'}}>
+                        <span>Information.exe</span>
+                        <Button style={{marginTop: '0.2rem'}}>X</Button>
+                    </WindowHeader>
+                    <WindowContent>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque, non.</p>
+                    </WindowContent>
+                </Window>
+            </div>
         </div>
     );
 };
