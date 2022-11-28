263e6bfbc1a1e0c188cf7d8bf8c93a3a1f11232d
mangoshi <soggymoose@gmail.com>
2022-11-28 18:22:12 +0000

    Started implementing the start menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,29 @@
-import { Link, useLocation} from 'react-router-dom';
-import { AppBar, Button, Frame, Toolbar} from 'react95';
-import { useContext, useState} from "react";
-import { GameContext } from "../GameContext";
+import {Link, useLocation} from 'react-router-dom';
+import {AppBar, Button, Frame, MenuList, MenuListItem, Separator, Toolbar} from 'react95';
+import {useContext, useState} from "react";
+import {GameContext} from "../GameContext";
 import Clock from 'react-live-clock';
 
 import ic_mangows from "./icons/ic_mangows.png"
 import ic_computer from "./icons/ic_computer.ico"
 import ic_msdos from "./icons/ic_msdos.ico"
 import ic_folder_exe from "./icons/ic_folder_exe.ico"
+import ic_user from "./icons/ic_user.ico"
+import ic_settings from "./icons/ic_settings.ico"
+import ic_auth from "./icons/ic_auth.ico"
 
 // TODO: Start menu with website settings (user account, theme, etc)
-const Navbar = ({ gameState }) => {
+const Navbar = ({gameState}) => {
 
 	// State for start menu
-	const [ open, setOpen ] = useState(false);
+	const [open, setOpen] = useState(false);
 
 	// Using GameContext to update viewed game tab
-	const { game } = useContext(GameContext)
+	const {game} = useContext(GameContext)
 
 	// Limiting game title to 20 characters in length
 	let limitedTitle = game
-	if (game && game.length > 20){
+	if (game && game.length > 20) {
 		limitedTitle = game.substring(0, 20) + "..."
 	}
 
@@ -49,30 +52,66 @@ const Navbar = ({ gameState }) => {
 						/>
 						Start
 					</Button>
+					{open && (
+						<MenuList
+							style={{
+								position: 'absolute',
+								left: '0',
+								top: '100%'
+							}}
+							onClick={() => setOpen(false)}
+						>
+							<MenuListItem>
+								<img
+									src={ic_user}
+									alt='mangows95 logo'
+									style={{height: '24px', marginRight: 4}}
+								/>
+								Profile
+							</MenuListItem>
+							<MenuListItem>
+								<img
+									src={ic_settings}
+									alt='mangows95 logo'
+									style={{height: '24px', marginRight: 4}}
+								/>
+								Settings
+							</MenuListItem>
+							<Separator/>
+							<MenuListItem>
+								<img
+									src={ic_auth}
+									alt='mangows95 logo'
+									style={{height: '24px', marginRight: 4}}
+								/>
+								Logout
+							</MenuListItem>
+						</MenuList>
+					)}
 					<Link to='/'>
 						<Button
-							active={location.pathname==='/'}
+							active={location.pathname === '/'}
 							size="lg"
 							style={{marginRight: '0.5rem'}}
 						>
 							<img
-							src={ic_computer}
-							alt='mangows95 logo'
-							style={{height: '24px', marginRight: 4}}
+								src={ic_computer}
+								alt='mangows95 logo'
+								style={{height: '24px', marginRight: 4}}
 							/>
 							Home
 						</Button>
 					</Link>
 					<Link to='/games/'>
 						<Button
-							active={location.pathname==='/games/'}
+							active={location.pathname === '/games/'}
 							size="lg"
 							style={{marginRight: '0.5rem'}}
 						>
 							<img
-							src={ic_folder_exe}
-							alt='mangows95 logo'
-							style={{height: '24px', marginRight: 4}}
+								src={ic_folder_exe}
+								alt='mangows95 logo'
+								style={{height: '24px', marginRight: 4}}
 							/>
 							Games
 						</Button>
