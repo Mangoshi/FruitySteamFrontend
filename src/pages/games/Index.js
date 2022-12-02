22e528f329303ff0d6cf85c3d550644d1ebdcb08
mangoshi <soggymoose@gmail.com>
2022-12-02 15:26:37 +0000

    Improved games index page logic
    - Query builder implemented
    - Search now mostly works
    - NSFW filter has been added

--- src/pages/games/Index.js
+++ src/pages/games/Index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import {useState, useEffect, useContext} from 'react';
 import {AuthContext} from "../../AuthContext";
 import GameCard from '../../components/GameCard';
-import {useParams} from "react-router-dom";
+import {json, Link, useParams} from "react-router-dom";
 import {
 	Button, Hourglass,
 	Select,
@@ -17,36 +17,89 @@ import {
 	WindowHeader
 } from "react95";
 
+import authenticatedOptions from './json/authenticatedSelectOptions.json'
+import unauthenticatedOptions from './json/unauthenticatedSelectOptions.json'
+
 const Index = () => {
+	// TODO: Figure out a way of allowing user to select specific page
+	// TODO: Allow user to specify page limit
+	// TODO: Allow user to remove NSFW filter (require age confirmation?)
+	const {page} = useParams();
 	const {token} = useContext(AuthContext)
 	const [games, setGames] = useState(null);
 	const [currentPage, setPage] = useState(1);
-	const {page} = useParams();
-	// TODO: Query builder! eg. if(bySelected){query+&by=by) [for each query type]
-	// TODO: Update page based on currentPage, perhaps use more than back/forward?
-	// TODO: Use "Notes" field to make NSFW filter [will need to update server too]
-	//  - Server will need second field in find() to filter by Notes having content
-	//  - Server's user schema will need DOB & showAdultContent bool
-	//  - NSFW filter will toggle based on bool, somehow
+	const [filterActive, setFilterActive] = useState(false)
+	const [searchBy, setSearchBy] = useState("Name")
+	const [searchQuery, setSearchQuery] = useState({query: ""})
+	const handleSearch = (e) => {
+		let name = e.target.name;
+		let value = e.target.value;
+
+		setSearchQuery(prevState => ({
+			...prevState,
+			[name]: value
+		}));
+	};
+
+	let URL, authHeaders, builtQuery
+
+	if(token){
+		URL = 'https://fruity-steam.vercel.app/api/games'
+		authHeaders = { headers: { "Authorization": `Bearer ${token}`}}
+	} else {
+		URL = 'https://fruity-steam.vercel.app/api/games/names'
+		authHeaders = ''
+	}
+
+	if(filterActive){
+		builtQuery = `&by=${searchBy}&query=${searchQuery.query}`
+	} else {
+		builtQuery = ''
+	}
+
+	let nsfwFilter = true
+
 	useEffect(() => {
-		axios.get(`https://fruity-steam.vercel.app/api/games/names?page=${currentPage}`)
+		axios.get(`${URL}?page=${currentPage}${builtQuery}`, authHeaders)
 			.then((response) => {
-				console.log(response.data);
-				setGames(response.data.data);
+				let data = response.data.data
+				if(nsfwFilter){
+/*
+					if(data.filter(game => game.Notes).length > 0) {
+						let count = 0
+						let i
+						console.group("Games with adult content:")
+						for (i in data) {
+							if (data[i].Notes) {
+								count++
+								console.log(count, "-", data[i].Name, ":", data[i].Notes)
+							}
+						}
+						console.groupEnd()
+						console.log("Filtered array", data.filter(game => !game.Notes))
+					}
+*/
+					setGames(data.filter(game => !game.Notes))
+					console.log(data.filter(game => !game.Notes))
+				} else {
+					setGames(data);
+					console.log(data);
+				}
 				// setPage(Number(page))
 			})
 			.catch((err) => {
 				console.error(err);
 			});
-	}, [currentPage]);
+	}, [URL, currentPage, builtQuery]);
 
 	if (!games) return (
 		<div style={{display: "flex", justifyContent: 'center'}}>
 			<Window style={{width: "250px"}}>
 				<WindowHeader style={{display: "flex", justifyContent: 'space-between'}}>
 					<span style={{marginLeft: '0.2rem'}}>Games.exe</span>
-					{/* TODO: Actually returns you to previous page */}
-					<Button style={{marginTop: '0.2rem'}}>X</Button>
+					<Link to='/'>
+						<Button style={{marginTop: '0.2rem'}}>X</Button>
+					</Link>
 				</WindowHeader>
 				<WindowContent>
 				<div style={{display: "flex", flexDirection: "column", justifyContent: 'center', alignItems: 'center'}}>
@@ -63,13 +116,11 @@ const Index = () => {
 	});
 
 	// Creating list of options for select box
-	const searchByOptions = []
-	for (const [key] of Object.entries(games[0])) {
-		let optionObject = {
-			value: key,
-			label: key
-		}
-		searchByOptions.push(optionObject)
+	let searchByOptions = []
+	if(token){
+		searchByOptions = authenticatedOptions
+	} else {
+		searchByOptions = unauthenticatedOptions
 	}
 
 	let unauthenticatedMessage
@@ -98,30 +149,40 @@ const Index = () => {
 				<Window style={{width: "auto"}}>
 					<WindowHeader style={{display: "flex", justifyContent: 'space-between'}}>
 						<span style={{marginLeft: '0.2rem'}}>Games.exe</span>
-						<Button style={{marginTop: '0.2rem'}}>X</Button>
+						<Link to='/'>
+							<Button style={{marginTop: '0.2rem'}}>X</Button>
+						</Link>
 					</WindowHeader>
 					<WindowContent>
 						{ unauthenticatedMessage }
+						<Button
+							style={{marginBottom: '1rem'}}
+							onClick={() => setFilterActive(!filterActive)}
+							active={filterActive}
+							fullWidth
+						>
+							Filter Results
+						</Button>
 						{/* Search div */}
-						<div style={{ display: 'flex', marginBottom: '1rem' }}>
-							<TextInput
-								placeholder='Type here...'
-								fullWidth
-							/>
-							<Select
-								defaultValue={"Name"}
-								width={250}
-								options={searchByOptions}
-								onChange={e => console.log('change', e)}
-								onOpen={e => console.log('open', e)}
-								onClose={e => console.log('close', e)}
-								onBlur={e => console.log('blur', e)}
-								onFocus={e => console.log('focus', e)}
-							/>
-							<Button onClick={e => console.log('click', e)} style={{ marginLeft: 4 }}>
-								Search
-							</Button>
-						</div>
+						{filterActive &&
+							<div style={{ display: 'flex', marginBottom: '1rem' }}>
+								<TextInput
+									placeholder='Type here...'
+									fullWidth
+									type="text"
+									name="query"
+									onChange={handleSearch}
+									value={searchQuery.query}
+								/>
+								<Select
+									defaultValue={"Name"}
+									width={250}
+									menuMaxHeight={200}
+									options={searchByOptions}
+									onChange={e => setSearchBy(e.value)}
+								/>
+							</div>
+						}
 						{/* Pagination div */}
 						<div style={{display: 'flex', justifyContent: 'space-between'}}>
 							{/* Previous Block*/}
